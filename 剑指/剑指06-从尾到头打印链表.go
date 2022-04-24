afad700a25cc22efb4f7075553d51fd121634163
lishuang <1136721473@qq.com>
2022-04-24 10:27:55 +0800

    消除报红

--- 剑指/剑指06-从尾到头打印链表.go
+++ 剑指/剑指06-从尾到头打印链表.go
@@ -1,12 +1,10 @@
 package main
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func reversePrint(head *ListNode) []int {
 	if head == nil {
 		return nil
